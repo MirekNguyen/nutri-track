8965e0775e3f1019d6dd2a1ccc67b8f70e27d9d0
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-04-13 22:32:30 +0200

    feat: Add Next.js configuration in TypeScript

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+export default nextConfig
