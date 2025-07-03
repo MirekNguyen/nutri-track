0939f99ad8363ef1b24668123f5dc70d31ffaf0e
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-07-03 21:01:39 +0200

    feat(modal): add meal fetching to modal dialog

--- src/app/@modal/(.)add/page.tsx
+++ src/app/@modal/(.)add/page.tsx
@@ -1,6 +1,7 @@
 import Modal from "@/components/modal";
 import { AddDialog } from "./add-dialog";
 import { format } from "date-fns";
+import { getMeals } from "@/app/actions/meal-actions";
 
 export default async function ModalPage({
   searchParams,
@@ -9,10 +10,11 @@ export default async function ModalPage({
 }) {
   const date = (await searchParams).date ?? new Date();
   const selectedDate = format(date ?? new Date(), "yyyy-MM-dd");
+  const meals = await getMeals();
 
   return (
     <Modal>
-      <AddDialog selectedDate={new Date(selectedDate)} meals={[]} />
+      <AddDialog selectedDate={new Date(selectedDate)} meals={meals} />
     </Modal>
   );
 }
