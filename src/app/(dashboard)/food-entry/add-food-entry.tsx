7132c50036d5f3f72add1818517f5e5fb935dc35
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-07-03 20:29:23 +0200

    refactor(food-entry): replace router with Link component

--- src/app/(dashboard)/food-entry/add-food-entry.tsx
+++ src/app/(dashboard)/food-entry/add-food-entry.tsx
@@ -2,22 +2,20 @@
 
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type Props = {
   selectedDate: string;
 };
 
 export const AddFoodEntry = ({ selectedDate }: Props) => {
-  const router = useRouter();
   return (
     <>
-      <Button
-        className="bg-green-600 hover:bg-green-700 text-sm w-full sm:w-auto"
-        onClick={() => router.push(`/add?date=${selectedDate}`)}
-      >
-        <PlusCircle className="mr-2 h-4 w-4" /> Add Food Entry
-      </Button>
+      <Link href={`/add?date=${selectedDate}`}>
+        <Button className="bg-green-600 hover:bg-green-700 text-sm w-full sm:w-auto">
+          <PlusCircle className="mr-2 h-4 w-4" /> Add Food Entry
+        </Button>
+      </Link>
     </>
   );
 };
