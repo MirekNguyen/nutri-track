597079326224af52485437654231646572fc763f
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-07-30 00:49:51 +0200

    feat(accordion): add accordion component for mobile view

--- src/components/dashboard/food-log/delete-entry-button.tsx
+++ src/components/dashboard/food-log/delete-entry-button.tsx
@@ -4,12 +4,14 @@ import { deleteFoodEntry } from "@/actions/food-entry-actions";
 import { Button } from "@/components/ui/button";
 import { FoodEntry } from "@/db/schema";
 import { Trash2 } from "lucide-react";
+import { PropsWithChildren } from "react";
 import { toast } from "sonner";
 
 type Props = {
   entry: FoodEntry;
-};
-export const DeleteEntryButton = ({ entry }: Props) => {
+  className?: string;
+} & PropsWithChildren;
+export const DeleteEntryButton = ({ entry, children, className }: Props) => {
   const handleDeleteEntry = async (id: number) => {
     try {
       await deleteFoodEntry(id);
@@ -32,9 +34,9 @@ export const DeleteEntryButton = ({ entry }: Props) => {
       variant="ghost"
       size="icon"
       onClick={() => handleDeleteEntry(entry.id)}
-      className="text-muted-foreground hover:text-red-500 hover:bg-red-50 dark:hover:bg-red-950"
+      className={`text-muted-foreground hover:text-red-500 hover:bg-red-50 dark:hover:bg-red-950 w-auto ${className}`}
     >
-      <Trash2 className="h-4 w-4" />
+      <Trash2 className="h-4 w-4" /> {children}
     </Button>
   );
 };
--- src/components/dashboard/food-log/food-record.tsx
+++ src/components/dashboard/food-log/food-record.tsx
@@ -1,6 +1,14 @@
+"use client";
 import { FoodEntry, Meal } from "@/db/schema";
 import { DeleteEntryButton } from "./delete-entry-button";
 import { format, parseISO } from "date-fns";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 type Props = {
   entry: FoodEntry;
@@ -21,6 +29,10 @@ const formatEntryDateTime = (dateStr: string, timeStr: string) => {
 };
 
 export const FoodRecord = ({ entry, meals }: Props) => {
+  const isMobile = useIsMobile();
+  if (isMobile) {
+    return <MobileFoodRecord entry={entry} meals={meals} />;
+  }
   return (
     <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center p-4 hover:bg-muted/50 transition-colors">
       <div className="flex items-center gap-3">
@@ -34,13 +46,15 @@ export const FoodRecord = ({ entry, meals }: Props) => {
                 ? meals.find((m) => m.id === entry.mealId)?.unit || "serving"
                 : "serving"}
             </span>
-              <span className="text-blue-600 dark:text-blue-400">
-                {entry.protein.toFixed(0)}g protein
-              </span>
-              <span className="text-purple-600 dark:text-purple-400">
-                {entry.carbs.toFixed(0)}g carbs
-              </span>
-              <span className="text-yellow-600 dark:text-yellow-400">{entry.fat.toFixed(0)}g fat</span>
+            <span className="text-blue-600 dark:text-blue-400">
+              {entry.protein.toFixed(0)}g protein
+            </span>
+            <span className="text-purple-600 dark:text-purple-400">
+              {entry.carbs.toFixed(0)}g carbs
+            </span>
+            <span className="text-yellow-600 dark:text-yellow-400">
+              {entry.fat.toFixed(0)}g fat
+            </span>
           </div>
         </div>
       </div>
@@ -49,6 +63,59 @@ export const FoodRecord = ({ entry, meals }: Props) => {
           {entry.calories.toFixed(0)} kcal
         </span>
         <DeleteEntryButton entry={entry} />
+      </div>
+    </div>
+  );
+};
+
+const MobileFoodRecord = ({ entry, meals }: Props) => {
+  return (
+    <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center px-4 hover:bg-muted/50 transition-colors">
+      <div>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value={entry.id.toString()}>
+            <AccordionTrigger>
+              <div className="flex items-center justify-between w-full">
+                <h3 className="font-medium text-foreground">
+                  {entry.foodName}
+                </h3>
+                <p>{entry.calories.toFixed(0)} kcal</p>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4 text-balance">
+              <div className="flex flex-col gap-1">
+              <div className="flex flex-wrap gap-x-3 text-xs text-muted-foreground mt-1">
+                <span>
+                  {formatEntryDateTime(entry.entryDate, entry.entryTime)}
+                </span>
+                <span className="font-medium">
+                  {entry.amount}{" "}
+                  {entry.mealId
+                    ? meals.find((m) => m.id === entry.mealId)?.unit ||
+                      "serving"
+                    : "serving"}
+                </span>
+              </div>
+              <div className="flex flex-wrap gap-x-3 text-xs text-muted-foreground">
+                <span className="text-blue-600 dark:text-blue-400">
+                  {entry.protein.toFixed(0)}g protein
+                </span>
+                <span className="text-purple-600 dark:text-purple-400">
+                  {entry.carbs.toFixed(0)}g carbs
+                </span>
+                <span className="text-yellow-600 dark:text-yellow-400">
+                  {entry.fat.toFixed(0)}g fat
+                </span>
+              </div></div>
+              <DeleteEntryButton
+                entry={entry}
+                className="border hover:border-red-600 dark:hover:border-red-400"
+              >
+                <p className="text-sm">Delete Entry</p>
+              </DeleteEntryButton>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </div>
   );
